94e54cef9b926b0f88c349f3dde8cdea024320fe
aa.kornev <aa.kornev@tensor.ru>
2017-09-05 15:52:36 +0300

    icons

--- dev/js/main.js
+++ dev/js/main.js
@@ -13,18 +13,17 @@ window.onload = function() {
             if (!files.length) {
                return;
             }
+            this.createCanvas();
             this.createImage(files[0]);
+
          },
-         loadCanvas: function() {
-            this.canvas = new fabric.Canvas(document.getElementById('canvas'), {
-               width: this.width,
-               height: this.height
-            });
+         loadImage: function() {
             var bg = new fabric.Image(document.getElementById('sourceImage'), {
                width: this.width,
-               height: this.height
+               height: this.height,
+               selectable: false
             });
-            this.canvas.add(bg).renderAll();
+            this.canvas.clear().add(bg).renderAll();
          },
          createImage: function(file) {
             var image = new Image();
@@ -35,7 +34,9 @@ window.onload = function() {
                image.onload = function() {
                   vm.width = this.naturalWidth;
                   vm.height = this.naturalHeight;
-                  vm.loadCanvas();
+                  vm.createCanvas();
+                  vm.setCanvasSize(vm.width, vm.height);
+                  vm.loadImage();
                };
                vm.sourceImage = image.src = e.target.result;
             };
@@ -51,8 +52,64 @@ window.onload = function() {
             this.canvas.lowerCanvasEl.toBlob(function(blob) {
                saveAs(blob, 'pretty image.png');
             });
+         },
+         createCanvas: function() {
+            if (!this.canvas) {
+               this.canvas = new fabric.Canvas(document.getElementById('canvas'), {});
+            }
 
+         },
+         setCanvasSize: function(width, height) {
+            this.canvas && this.canvas.setDimensions({
+               width: width,
+               height: height
+            });
          }
+
       }
+   });
+   var panel = new Vue({
+      el: '#commandPanel',
+      data: {
+         tools: [
+            {
+               icon: 'icon icon-arrow-up-right2 commandPanel__item',
+               id: 'arrow',
+               title: 'Arrow'
+            },
+            {
+               icon: 'icon icon-checkbox-unchecked commandPanel__item',
+               id: 'rect',
+               title: 'Rectangle'
+            },
+            {
+               icon: 'icon icon-font-size commandPanel__item',
+               id: 'Text',
+               title: 'Text'
+            },
+            {
+               icon: 'icon icon-pencil2 commandPanel__item',
+               id: 'freedraw',
+               title: 'Freedraw'
+            },
+            {
+               icon: 'icon icon-crop commandPanel__item',
+               id: 'crop',
+               title: 'crop'
+            },
+            {
+               icon: 'icon icon-scissors commandPanel__item',
+               id: 'scissors',
+               title: 'Scissors'
+            },
+            {
+               icon: 'icon icon-droplet commandPanel__item',
+               id: 'blur',
+               title: 'Blur'
+            }
+         ]
+
+      }
+
    });
 };

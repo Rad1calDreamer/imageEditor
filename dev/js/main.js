a87fcf2b1d7a7cb37a1c3dcf1e55b2ac9a0e1acf
aa.kornev <aa.kornev@tensor.ru>
2017-09-29 11:12:10 +0300

    tools start

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,22 @@
 window.onload = function() {
+   const EventBus = new Vue({
+      created() {
+         this.$on('my-event', this.handleMyEvent);
+      },
+      methods: {
+         handleMyEvent($event) {
+            console.log('My event caught in global event bus', $event);
+         }
+      }
+   });
+   Object.defineProperties(Vue.prototype, {
+      $bus: {
+         get: function() {
+            return EventBus;
+         }
+      }
+   });
+
    var app = new Vue({
       el: '#imageEditor',
       data: {
@@ -6,6 +24,57 @@ window.onload = function() {
          canvas: null,
          width: 300,
          height: 150
+      },
+      created() {
+         this.$bus.$on('onToolClick', (tool, state) => {
+            this.toolClick(tool, state);
+            let canvas = this.canvas,
+               origX, origY, rect, newObj, _drawHandler;
+            canvas.on('mouse:down', function(option) {
+               if (option.target && (option.target.selectable || option.target.isEditing)) {
+                  return null;
+               } else {
+                  let pointer = canvas.getPointer(option.e);
+                  origX = pointer.x;
+                  origY = pointer.y;
+                  newObj = true;
+                  rect = new fabric.Rect({
+                     left: origX,
+                     top: origY,
+                     width: pointer.x - origX,
+                     height: pointer.y - origY,
+                     fill: 'transparent',
+                     stroke: 'red',
+                     strokeWidth: 6
+                  });
+
+                  canvas.add(rect);
+                  _drawHandler = function(option) {
+                     if (option.e.touches && option.e.touches.length > 1) {
+                        return;
+                     }
+                     let pointer = canvas.getPointer(option.e);
+                     if (origX > pointer.x) {
+                        rect.setLeft(pointer.x);
+                     }
+                     if (origY > pointer.y) {
+                        rect.setTop(pointer.y);
+                     }
+                     rect.setWidth(Math.abs(origX - pointer.x));
+                     rect.setHeight(Math.abs(origY - pointer.y));
+                     rect.setCoords();
+                     canvas.renderAll();
+                  };
+                  canvas.on('mouse:move', _drawHandler);
+               }
+            });
+
+            canvas.on('mouse:up', function() {
+               canvas.off('mouse:move', _drawHandler);
+            });
+
+
+         });
       },
       methods: {
          onFileChange: function(e) {
@@ -64,8 +133,10 @@ window.onload = function() {
                width: width,
                height: height
             });
+         },
+         onToolClick: function() {
+            console.log(arguments);
          }
-
       }
    });
    var panel = new Vue({
@@ -75,40 +146,56 @@ window.onload = function() {
             {
                icon: 'icon icon-arrow-up-right2 commandPanel__item',
                id: 'arrow',
-               title: 'Arrow'
+               title: 'Arrow',
+               state: false
             },
             {
                icon: 'icon icon-checkbox-unchecked commandPanel__item',
                id: 'rect',
-               title: 'Rectangle'
+               title: 'Rectangle',
+               state: false
             },
             {
                icon: 'icon icon-font-size commandPanel__item',
                id: 'Text',
-               title: 'Text'
+               title: 'Text',
+               state: false
             },
             {
                icon: 'icon icon-pencil2 commandPanel__item',
                id: 'freedraw',
-               title: 'Freedraw'
+               title: 'Freedraw',
+               state: false
             },
             {
                icon: 'icon icon-crop commandPanel__item',
                id: 'crop',
-               title: 'crop'
+               title: 'crop',
+               state: false
             },
             {
                icon: 'icon icon-scissors commandPanel__item',
                id: 'scissors',
-               title: 'Scissors'
+               title: 'Scissors',
+               state: false
             },
             {
                icon: 'icon icon-droplet commandPanel__item',
                id: 'blur',
-               title: 'Blur'
+               title: 'Blur',
+               state: false
             }
          ]
 
+      },
+      methods: {
+         toolClick: function(name, state) {
+            this.$bus.$emit('onToolClick', name, state);
+            var toolIdx = this.tools.findIndex(function(element) {
+               return element.title === name;
+            });
+            this.tools[toolIdx].state = !state;
+         }
       }
 
    });
